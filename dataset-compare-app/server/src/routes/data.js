6f74ee78c2ad092513f41a350dd02042ff1eb8ff
Akash Jasrotia <akashjasrotia2005@gmail.com>
2026-03-20 16:30:17 +0530

    deleted

--- dataset-compare-app/server/src/routes/data.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDataById } from '../controllers/dataController.js';
-
-const router = express.Router();
-
-// Route to fetch data by ID
-router.get('/data/:id', getDataById);
-
-export default router;
